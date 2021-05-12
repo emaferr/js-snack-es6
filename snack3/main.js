50a5c0665017a097a80885c2588184a19d21243e
ema.ferr <78190534+emaferr@users.noreply.github.com>
2021-05-12 22:26:53 +0200

    Aggiunta funzione universale

--- snack3/main.js
+++ snack3/main.js
@@ -1,28 +1,53 @@
 // Scrivere una funzione che accetti tre argomenti, un array e due numeri (a più piccolo di b).
-// Usiamo i nuovi metodi degli array foreach o filter.
-// La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa tra i due numeri.
 
 const parole = [ "casa" , "scuola" , "albero" , "mare" , "montagna" , "sole"] ;
 
-const nuovoArray = [] ;
+function arrayContenuto ( array , valoreMin , valoreMax ) {
 
-let numeroA= 1 ;
+    const nuovoArray = [] ;
 
-let numeroB = 6 ;
+    // Usiamo i nuovi metodi degli array foreach o filter.
 
-const arrayContenuto = parole.forEach( ( parola , i ) => {
+    array.forEach( ( elemento , index ) => {
 
-    if ( i < numeroB - 1 && i >= numeroA ) {
+        if ( index < valoreMax - 1 && index >= valoreMin ) {
 
-        nuovoArray.push(parola)
+          nuovoArray.push( elemento )
 
-    }
+        }
+        
+    });
 
-} )
+    // La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa tra i due numeri.
+
+    return nuovoArray
+}
+
+let risultato = arrayContenuto( parole , 2 , 6 ) ;
+
+console.log(risultato);
 
 
-console.log(nuovoArray);
+// Metodo con arrow function //
 
+// const nuovoArray = [] ;
+
+// let numeroA= 1 ;
+
+// let numeroB = 6 ;
+
+// const arrayContenuto = parole.forEach( ( parola , i ) => {
+
+//     if ( i < numeroB - 1 && i >= numeroA ) {
+
+//         nuovoArray.push(parola)
+
+//     }
+
+// } )
+
+
+// console.log(nuovoArray);
 
 
 
